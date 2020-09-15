a2ca088a4a15d229ba5b46368e6549dc23184b86
Mcfly2340 <mcfly2340@gmail.com>
2020-09-15 12:45:25 +0200

    Changed Background to White, First name's position, and color to Red and Last name to Blue For 2.2

--- src/h02/Show2.java
+++ src/h02/Show2.java
@@ -4,17 +4,19 @@ import java.awt.*;
 import java.applet.*;
 
 //een klasse met de naam Show van het type Applet
-public class Show extends Applet {
+public class Show2 extends Applet {
 
     //een (lege) methode die de Applet initialiseert
     public void init() {
-        setBackground(Color.BLUE);
+        setBackground(Color.WHITE);
     }
     //een methode die de inhoud van het scherm tekent
     public void paint(Graphics g) {
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.BLUE);
         g.setFont(new Font("Comic Sans MS", Font.BOLD, 30));
         g.drawString("Rick", 150, 130 );
+        g.setColor(Color.RED);
+        g.drawString("Slierendregt", 150, 170 );
 
     }
 
