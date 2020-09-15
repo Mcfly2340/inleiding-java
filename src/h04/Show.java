e4b25531f199d363d9180dc061b25588d10c14e0
Mcfly2340 <mcfly2340@gmail.com>
2020-09-15 13:05:38 +0200

    Added 4.1 created triangle

--- src/h04/Show.java
+++ src/h04/Show.java
@@ -13,13 +13,9 @@ public class Show extends Applet {
     public void paint(Graphics g) {
         setBackground(Color.WHITE);
         g.setColor(Color.BLACK);
-        g.drawRect(20, 20, 150, 400);
-        g.drawOval(45, 45, 100, 100);
-        g.drawOval(45, 160, 100, 100);
-        g.drawOval(45, 285, 100, 100);
-        g.drawArc(169, 28, 100, 50, 90, 10);
-        g.drawArc(213, 13, 160, 50, 160, 10);
-        g.drawArc(210, 0, 160, 50, 190, 10);
+        g.drawLine(20, 200, 200, 200);
+        g.drawLine(20, 200, 100, 50);
+        g.drawLine(200, 200, 100, 50);
 
     }
 }
