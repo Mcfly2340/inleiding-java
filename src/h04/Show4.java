f49d6d497eb921751be2ed186af22d77b5a8cc14
Mcfly2340 <mcfly2340@gmail.com>
2020-09-15 12:50:38 +0200

    Changed Background to White, First name's position, and color to Red and Last name to Blue For 2.2

--- src/h04/Show4.java
+++ src/h04/Show4.java
@@ -15,11 +15,13 @@ public class Show4 extends Applet {
         g.setColor(Color.BLACK);
         //valerie
         g.drawString("Valerie", 40, 200 );
-        g.fillRect(50, 140, 25, 40);
+        g.fillRect(50, 150, 25, 30);
         //jeroen
         g.drawString("Jeroen", 100, 200 );
+        g.fillRect(105, 100, 25, 80);
         //hans
         g.drawString("Hans", 160, 200 );
+        g.fillRect(160, 115, 25, 65);
         //100
         g.drawString("100", 20, 100 );
         //80
