f3c47ae428d0e515160bcc385d28a97dfb87b7db
Mcfly2340 <mcfly2340@gmail.com>
2020-09-21 11:08:04 +0200

    Changed Chapter 8, edited Assignment 8.1

--- src/h08/Reset.java
+++ src/h08/Reset.java
@@ -8,26 +8,41 @@ import java.awt.event.*;
 
 public class Reset extends Applet {
     TextField tekstvak;
-    TextField reset;
     Button knop;
+    Label label;
+    double getal;
 
     public void init() {
         tekstvak = new TextField("", 30);
         knop = new Button("Reset");
-        knop.addActionListener(new KnopListener());
+        knop.addActionListener( new NewListener() );
         add(tekstvak);
         add(knop);
-        //Reset Knop
+        label = new Label("Search");
+        tekstvak.addActionListener( new Reset.KnopListener() );
+        add(label);
+        add(tekstvak);
         knop = new Button("Ok");
-        knop.addActionListener(new KnopListener());
+        knop.addActionListener( new KnopListener() );
         add(tekstvak);
         add(knop);
     }
 
+    class NewListener implements ActionListener	{
+        public void actionPerformed( ActionEvent e ) {
+            tekstvak.setText("");
+            repaint();
+        }
+    }
+    public void paint(Graphics g) {
+        g.drawString("Het getal is " + getal, 50, 60 );
+    }
     class KnopListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            tekstvak.setText("Jammer, " +
-                    "maar nu staat er iets anders");
+            String s = tekstvak.getText();
+            getal = Double.parseDouble( s );
+            //Je kan deze regels ook samenvoegen
+            //getal = Double.parseDouble(tekstvak.getText());
             repaint();
         }
     }
