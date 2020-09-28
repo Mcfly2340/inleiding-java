9168cc94eba2ea3d7785ecc76f01e4dfa9dff4c3
Mcfly2340 <mcfly2340@gmail.com>
2020-09-28 13:25:41 +0200

    Changed Chapter 10, and finished 10.1 and 10.2

--- src/h10/Grootste.java
+++ src/h10/Grootste.java
@@ -1,24 +1,21 @@
 package h10;
-
 //opdracht 10.1
 
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
 
-
 public class Grootste extends Applet {
-
     TextField tekstvak;
     Label label;
-    String s, tekst;
-    int jaartal;
-    int uitkomst;
+    String textFromTekstvak, tekst; //bijvoorbeeld "hallo"
+    int jaartal; //variabele met de naam "jaartal" en als dataype 'int' (dus er mogen alleen hele getallen in deze variabele)
+    int grootsteJaartal = 0; //variabele met de naam "grootsteJaartal" ,et als dataype 'int' en deze hebben we vast een waarde gegeven namelijk 0
 
     public void init() {
         tekstvak = new TextField("", 20);
         label = new Label("Type een jaartal en druk op enter");
-        tekstvak.addActionListener( new Grootste.TekstvakListener() );
+        tekstvak.addActionListener( new TekstvakListener() );
         tekst = "";
         add(label);
         add(tekstvak);
@@ -30,14 +27,17 @@ public class Grootste extends Applet {
 
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            s = tekstvak.getText();
-            jaartal = Integer.parseInt( s);
-            if (uitkomst % jaartal > jaartal) {
-                tekst = ""+ jaartal + " is een schrikkeljaar";
+            textFromTekstvak = tekstvak.getText();
+            jaartal = Integer.parseInt(textFromTekstvak); //hier komt de waarde van wat de gebru8iker heeft ingevoerd in het tekstvak
+            //System.out.println(jaartal);
+            if (grootsteJaartal > jaartal) {
+                //hier hoeven we niets veranderne, want het grootsteJaartal blijft het grootsteJaartal
             }
             else {
-                tekst = ""+ uitkomst;
+                grootsteJaartal = jaartal; //het nieuwe grootsteJaartal is hjet getal dat we net hebben ingeoerd (jaartal)
+
             }
+            tekst= "Het grootste getal dat we hebben ingevoerd is : " + grootsteJaartal;
             repaint();
         }
     }
--- src/h10/Grootste2.java
+++ src/h10/Grootste2.java
@@ -2,7 +2,62 @@ package h10;
 
 //opdracht 10.2
 
-public class Grootste2 {
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
 
+public class Grootste2 extends Applet {
+    TextField tekstvak;
+    Label label;
+    String textFromTekstvak, tekst, tekst2; //bijvoorbeeld "hallo"
+    int jaartal; //variabele met de naam "jaartal" en als dataype 'int' (dus er mogen alleen hele getallen in deze variabele)
+    int grootsteJaartal = 0; //variabele met de naam "grootsteJaartal" ,et als dataype 'int' en deze hebben we vast een waarde gegeven namelijk 0
+    int kleinsteJaartal = 0;
 
+    public void init() {
+        tekstvak = new TextField("", 20);
+        label = new Label("Type een jaartal en druk op enter");
+        tekstvak.addActionListener( new TekstvakListener() );
+        tekstvak.addActionListener( new Tekstvak2Listener() );
+        tekst = "";
+        add(label);
+        add(tekstvak);
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(tekst, 50, 60 );
+        g.drawString(tekst2, 50, 100 );
+    }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            textFromTekstvak = tekstvak.getText();
+            jaartal = Integer.parseInt(textFromTekstvak); //hier komt de waarde van wat de gebru8iker heeft ingevoerd in het tekstvak
+            //System.out.println(jaartal);
+            if (grootsteJaartal > jaartal) {
+                //hier hoeven we niets veranderne, want het grootsteJaartal blijft het grootsteJaartal
+            }
+            else {
+                grootsteJaartal = jaartal; //het nieuwe grootsteJaartal is hjet getal dat we net hebben ingeoerd (jaartal)
+
+            }
+            tekst= "Het grootste getal dat we hebben ingevoerd is : " + grootsteJaartal;
+            repaint();
+        }
+    }
+    class Tekstvak2Listener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            textFromTekstvak = tekstvak.getText();
+            jaartal = Integer.parseInt(textFromTekstvak);
+            System.out.println(kleinsteJaartal);
+            if (kleinsteJaartal < jaartal) {
+            }
+            else {
+                kleinsteJaartal = jaartal;
+
+            }
+            tekst2= "Het kleinste getal dat we hebben ingevoerd is : " + kleinsteJaartal;
+            repaint();
+        }
+    }
 }
