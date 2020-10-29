45eaf96e7d3f09f8af192771c3dac3d6725e4fd6
Mcfly2340 <mcfly2340@gmail.com>
2020-10-29 12:28:17 +0100

    Finished 13.2, added 13.3

--- src/h13/Bakstenen.java
+++ src/h13/Bakstenen.java
@@ -1,19 +1,35 @@
 package h13;
-
+//13.2
 import java.applet.Applet;
 import java.awt.*;
 
 public class Bakstenen extends Applet {
-    double teller;
+    double horizontaal;
     double d;
 
-    public void paint(Graphics g){
-        for(teller=0; teller<10; teller+=35) {
-            tekenBaksteen(g, 20, 20, 50, 20);
-        }
+    public void paint(Graphics g) {
+        tekenBaksteen(g, 20, 20, 50, 15);
     }
-    void tekenBaksteen( Graphics g, int x1, int y1, int w1, int h1){
-        g.setColor(Color.RED);
-        g.fillRect(x1, y1, w1, h1);
+
+    void tekenBaksteen(Graphics g, int x1, int y1, int w1, int h1) {
+        for (horizontaal = 0; horizontaal < 20; horizontaal++) {
+            if (horizontaal % 2 == 0) {
+                x1=20;
+                for (int verticaal = 0; verticaal < 20; verticaal++) {
+                    g.setColor(Color.RED);
+                    g.fillRect(x1, y1, w1, h1);
+                    x1+=w1+10;
+                }
+                y1 += 20;
+            } else {
+                x1=50;
+                for(int verticaal=0; verticaal<20; verticaal++){
+                    g.setColor(Color.RED);
+                    g.fillRect(x1, y1, w1, h1);
+                    x1+=w1+10;
+                }
+                y1 += 20;
+            }
+        }
     }
 }
---
+++ src/h13/Muren.java
@@ -0,0 +1,13 @@
+package h13;
+//13.3
+import java.applet.Applet;
+import java.awt.*;
+
+public class Muren extends Applet {
+     public void init(){
+
+     }
+     public void paint(Graphics g){
+
+     }
+}
